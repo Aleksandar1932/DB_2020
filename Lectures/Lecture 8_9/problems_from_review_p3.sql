b1bef2cf9f5d80bf71aac6812936c70e793cf6d2
Aleksandar1932 <aleksandar.ivanovski123@gmail.com>
2020-12-19 16:49:13 +0100

    Start Review 10 part 3
    Start the problems which are going to be covered in part 3 of the review for the lecture 9 (on 21.12.2020)
    Solve problems 86 - 100 and compared the reulsts with the slides

---
+++ Lectures/Lecture 8_9/problems_from_review_p3.sql
@@ -0,0 +1,113 @@
+--86
+select *
+from customer
+where exists(select * from customer where city in ('London'));
+
+--87
+select *
+from salesman
+where salesman_id in (select distinct salesman_id
+                      from customer a
+                      where exists(select *
+                                   from customer b
+                                   where b.salesman_id = a.salesman_id
+                                     and b.cust_name <> a.cust_name));
+
+select *
+from salesman
+where salesman_id in (select salesman.salesman_id as num_custs
+                      from salesman,
+                           customer
+                      where salesman.salesman_id = customer.salesman_id
+                      group by salesman.salesman_id
+                      having count(customer.customer_id) > 1);
+
+--88
+select *
+from salesman
+where salesman_id in (select salesman.salesman_id
+                      from salesman,
+                           customer
+                      where salesman.salesman_id = customer.salesman_id
+                      group by salesman.salesman_id
+                      having count(customer.customer_id) = 1);
+
+--89
+select *
+from salesman
+where salesman_id in
+      (select salesman.salesman_id
+       from salesman,
+            customer,
+            orders
+       where salesman.salesman_id = customer.salesman_id
+         and customer.customer_id = orders.customer_id
+       group by salesman.salesman_id, customer.customer_id
+       having count(orders.customer_id) > 1
+      );
+
+--90
+select *
+from salesman as a
+where exists(select * from customer b where a.name < b.cust_name);
+
+--91
+select *
+from salesman as s
+where exists(select * from customer as a where a.city = s.city);
+
+--92
+select *
+from customer
+where grade > (select avg(grade) from customer where city = 'New York');
+
+--93
+select *
+from orders
+where purch_amt > any (select purch_amt from orders where ord_date = '2012-09-10');
+
+--94
+select *
+from orders
+where purch_amt < any (select purch_amt
+                       from orders,
+                            customer
+                       where orders.customer_id = customer.customer_id
+                         and customer.city = 'London');
+
+--95
+select *
+from orders
+where purch_amt < (select max(orders.purch_amt)
+                   from orders,
+                        customer
+                   where orders.customer_id = customer.customer_id
+                     and customer.city = 'London');
+
+--96
+select *
+from customer
+where grade > all (select grade from customer where city = 'New York');
+
+--97
+select *
+from customer
+where grade <> all (select grade from customer where city = 'London');
+
+--98
+select *
+from customer
+where grade <> all (select grade from customer where city = 'Paris');
+
+--99
+select *
+from customer
+where grade <> all (select grade from customer where city = 'Dallas');
+
+--100
+select name, city, total
+from (select a.salesman_id as id, sum(purch_amt) as total
+      from (select * from salesman where city in (select city from customer)) as a
+               join orders on a.salesman_id = orders.salesman_id
+      group by a.salesman_id) as b
+         join salesman on b.id = salesman.salesman_id;
